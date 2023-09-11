0ace05cd38a6e2edfbacea59d52badde8efaaf45
SreekanthGorantla <gorantlasreekanth@gmail.com>
2023-09-11 21:16:14 +0530

    myfilecommit1

---
+++ myfile.py
@@ -0,0 +1,46 @@
+print("Hello, World")
+
+if 5 < 2:
+    print("Five is greater than two!")
+else:
+    print("False.")
+    
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+#Camel Cas: Each word, except the first, started with a capital letter
+
+myVariableName = "Sree"
+print("myVariableName is : " + myVariableName)
+
+#Pascal Case: Each word starts with a capital letter
+
+MyVariableName = "Sree"
+print("MyVariableName is : " + MyVariableName)
+
+#Snake Case: Each word is seperated by an underscore character:
+
+my_variable_name = "Sree"
+print("my_variable_name is : " + my_variable_name)
+
+# Python allows you to assign values to multiple variables in one line:
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x + ", " + y + ", " + z)
